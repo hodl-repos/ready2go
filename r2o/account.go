a9ac55c9415760961683d5bdf9913e74749d5456
SM <simon@mauracher.cc>
2022-04-29 21:12:57 +0200

    updated account-service, added request-dto

--- r2o/account.go
+++ r2o/account.go
@@ -11,7 +11,7 @@ import (
 // ready2order API docs: https://app.swaggerhub.com/apis-docs/ready2ordergmbh/ready2order-api-production/1.0.329#/
 type AccountService service
 
-type Account struct {
+type AccountGetResponse struct {
 	CompanyID                  *int    `json:"company_id"`
 	CompanyBranch              *int    `json:"company_branch"`
 	CompanyUsername            *string `json:"company_username"`
@@ -38,8 +38,35 @@ type Account struct {
 	CompanyPartnerData         *string `json:"company_partnerData"`
 }
 
-func (as *AccountService) GetAccountInfo(ctx context.Context) (*Account, error) {
-	responseData := Account{}
+type AccountPostRequest struct {
+	CompanyAccountNumber       string `json:"company_accountNumber"`
+	CompanyBranch              int64  `json:"company_branch"`
+	CompanyBusiness            string `json:"company_business"`
+	CompanyBusinessPhoneNumber string `json:"company_businessPhoneNumber"`
+	CompanyCity                string `json:"company_city"`
+	CompanyFailedLoginAttempts int64  `json:"company_failedLoginAttempts"`
+	CompanyFirstName           string `json:"company_firstName"`
+	CompanyID                  int64  `json:"company_id"`
+	CompanyLastName            string `json:"company_lastName"`
+	CompanyLiveMode            bool   `json:"company_liveMode"`
+	CompanyLiveModeStartedAt   string `json:"company_liveModeStartedAt"`
+	CompanyName                string `json:"company_name"`
+	CompanyPartnerData         string `json:"company_partnerData"`
+	CompanyPhone               string `json:"company_phone"`
+	CompanyReferrer            string `json:"company_referrer"`
+	CompanyStreet              string `json:"company_street"`
+	CompanyTrainingsMode       bool   `json:"company_trainingsMode"`
+	CompanyUsername            string `json:"company_username"`
+	CompanyVatID               string `json:"company_vatId"`
+	CompanyWebsite             string `json:"company_website"`
+	CompanyZip                 string `json:"company_zip"`
+	CountryID                  string `json:"country_id"`
+	CurrencyID                 int64  `json:"currency_id"`
+	LanguageID                 int64  `json:"language_id"`
+}
+
+func (as *AccountService) GetAccountInfo(ctx context.Context) (*AccountGetResponse, error) {
+	responseData := AccountGetResponse{}
 
 	err := as.client.runHttpRequestWithContext(ctx, "company", http.MethodGet, nil, &responseData)
 
@@ -50,6 +77,10 @@ func (as *AccountService) GetAccountInfo(ctx context.Context) (*Account, error) 
 	return &responseData, nil
 }
 
-func (as *AccountService) UpdateAccountInfo(ctx context.Context, account *Account) error {
+func (as *AccountService) UpdateAccountInfo(ctx context.Context, account *AccountPostRequest) error {
 	return as.client.runHttpRequestWithContext(ctx, "company", http.MethodPost, account, nil)
+}
+
+func (as *AccountService) MapCustomerResponseToRequest(account *AccountGetResponse) AccountPostRequest {
+	return AccountPostRequest{}
 }
